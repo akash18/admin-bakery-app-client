ea0c2f498f84651c0cc632f78954f318a740ffa8
Akash Lalwani <alalwani@adobe.com>
2017-09-23 23:04:48 +0530

    first commit

--- src/app/environment-variables/development.ts
+++
@@ -1,7 +0,0 @@
-export const devVariables = {
-  apiEndpoint: 'http://192.168.0.8:8083/',
-  environmentName: 'Development Environment',
-  ionicEnvName: 'dev',
-  gcmSenderId: '1094873210402',
-  appId:'0112eabb'
-};
